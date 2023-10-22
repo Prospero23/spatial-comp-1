48fed41568b784f0a145289610a1b239336a8502
Ben Eidson <b.e.eidson@gmail.com>
2023-10-22 10:29:14 -0400

    basic ascii effect mess around and script to run webpack in watch mode

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,19 @@
 import * as THREE from "three";
+import { AsciiEffect } from 'three/addons/effects/AsciiEffect.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+
+const effect = new AsciiEffect( renderer, ' .:-+*=%@#', { invert: true } );
+effect.setSize( window.innerWidth, window.innerHeight );
+effect.domElement.style.color = 'white';
+effect.domElement.style.backgroundColor = 'black';
+
+document.body.appendChild( effect.domElement );
 
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -16,7 +24,13 @@ scene.add( cube );
 camera.position.z = 5;
 
 function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+	effect.render( scene, camera );
 }
 animate();
+
+// to recompile bundle
+// npx webpatch --watch
+// webpack --watch 

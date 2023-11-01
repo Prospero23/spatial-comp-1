cb862d106b7c8684a0542681cc26248adb188e56
Ben Eidson <b.e.eidson@gmail.com>
2023-11-01 15:41:20 -0400

    initialized threeJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
-function App() {
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
 
+function App() {
   return (
-    <>
-    <h1 className="text-xl underline">TEST</h1>
-     </>
-  )
+    <main className="h-screen w-screen">
+    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+      <color attach="background" args={["#ececec"]} />
+      <Experience />
+    </Canvas>
+    </main>
+  );
 }
 
-export default App
+export default App;
+// vitest
